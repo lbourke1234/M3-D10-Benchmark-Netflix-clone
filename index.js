12faabd31190d889961bf2e80fbfa93e8655cacb
lbourke1234 <lbourke1234@gmail.com>
2022-04-15 13:45:36 +0100

    Break

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 window.onload = () => {
     // getMovies()
+    fillThriller()
+    fillComedy()
+    fillDrama()
+    if (location.href === 'back.html'){
+        removeDefaultFormSubmit()
+    }
 }
 
 const getMovies = () => {
@@ -14,18 +20,23 @@ const getMovies = () => {
     })
 }
 
-const handleFormSubmit = async (e) => {
-    try{
+const removeDefaultFormSubmit = () => {
+    document.getElementById('form').addEventListener('submit', e => {
         e.preventDefault()
+    })
+}
+
+
+
+const postMovie = async () => {
+    try{
     
-        console.log(e)
         const myObject = {
             name: document.getElementById('name').value,
             description: document.getElementById('description').value,
             category: document.getElementById('category').value,
             imageUrl: document.getElementById('imageUrl').value
         }
-        console.log('before fetch')
         let response = await fetch('https://striveschool-api.herokuapp.com/api/movies', {
             method: 'POST',
             body: JSON.stringify(myObject),
@@ -36,20 +47,96 @@ const handleFormSubmit = async (e) => {
         })
     
     if (response.ok) {
-        const body = response.json()
+        const body = await response.json()
 
         console.log(body)
+        alert('Movie with ID: ' + body._id + ' added successfully')
+
     }
+    
     console.log('last')
     } catch (err) {
         console.log(err)
     }
 }
 
-const postMovie = (currentObjectValue) => {
+const updateMovies = () => {
     
 }
 
-const updateMovies = () => {
+const fillThriller = async () => {
+    let row = document.getElementById('thriller-row')
+    row.innerHTML = ''
+
+    const response = await fetch('https://striveschool-api.herokuapp.com/api/movies/Thriller', {
+        headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NDk4NTE2NzcsImV4cCI6MTY1MTA2MTI3N30.K2GETfU9WC5AqVtF5vXMgGcBW4A-AYjnC8rZPl53d_U'
+        }
+    })
+    const body = await response.json()
+    console.log(body)
+
     
+    
+    for (let i = 0; i < 6; i++) {
+        row.innerHTML += `
+                <div id="${body[i]._id}" class="col-sm-6 col-md-4 col-lg-2 px-1">
+                  <img  class="d-block w-100 img-fluid" src="${body[i].imageUrl}" alt=" First slide" onclick='goToBack(${body})'>
+                </div>
+        `
+    }
+
+}
+
+const fillComedy = async () => {
+    let row = document.getElementById('comedy-row')
+    row.innerHTML = ''
+
+    const response = await fetch('https://striveschool-api.herokuapp.com/api/movies/Thriller', {
+        headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NDk4NTE2NzcsImV4cCI6MTY1MTA2MTI3N30.K2GETfU9WC5AqVtF5vXMgGcBW4A-AYjnC8rZPl53d_U'
+        }
+    })
+    const body = await response.json()
+    console.log(body)
+
+    
+    
+    for (let i = 0; i < 6; i++) {
+        row.innerHTML += `
+                <div class="col-sm-6 col-md-4 col-lg-2 px-1">
+                  <img class="d-block w-100 img-fluid" src="${body[i].imageUrl}" alt=" First slide">
+                </div>
+        `
+    }
+
+}
+
+const fillDrama = async () => {
+    let row = document.getElementById('drama-row')
+    row.innerHTML = ''
+
+    const response = await fetch('https://striveschool-api.herokuapp.com/api/movies/Thriller', {
+        headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NDk4NTE2NzcsImV4cCI6MTY1MTA2MTI3N30.K2GETfU9WC5AqVtF5vXMgGcBW4A-AYjnC8rZPl53d_U'
+        }
+    })
+    const body = await response.json()
+    console.log(body)
+
+    
+    
+    for (let i = 0; i < 6; i++) {
+        row.innerHTML += `
+                <div class="col-sm-6 col-md-4 col-lg-2 px-1">
+                  <img class="d-block w-100 img-fluid" src="${body[i].imageUrl}" alt=" First slide">
+                </div>
+        `
+    }
+
+}
+
+const goToBack = (id) => {
+    console.log('hello')
+    // location.href = 'back.html' + id
 }
